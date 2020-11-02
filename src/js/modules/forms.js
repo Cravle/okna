11c4633cd4c5d0fb28892603cd01e117da2c9d93
Loboda Vladyslav <vladoxer28502@gmail.com>
2020-11-02 19:27:53 +0200

    reworked form, add post new infromation: type, width height etc from modal_end

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,14 +1,12 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+//TODO сделать так чтобы не пускало на следующую модалку без выбора всех атрибутов
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-        inputs = document.querySelectorAll('input'),
-        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+        inputs = document.querySelectorAll('input');
 
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');//можно писать только цифры
-        });
-    });
+    checkNumInputs('input[name="user_phone"]');
+
 
     const message = {
         loading: 'Загрузка...',
@@ -43,6 +41,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') == "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
             postData('assets/server.php', formData)
                 .then(res => {
                     console.log(res);
